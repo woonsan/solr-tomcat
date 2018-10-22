2aa7e338975f76072684dc481d889bb360080df2
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-10-21 23:19:46 -0400

    adding solr request filter

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # solr-tomcat
 
+PoC to embed Apache Tomcat 9 for Apache Solr 7.
+
 ## How to build
 
     $ mvn clean package
 
 ## How to run
 
-    $ cd target
-    $ bin/solrserver
+```
+$ cd target
+$ bin/solrserver
+usage: solrserver
+ -c,--context-path <arg>   Application context path
+ -d,--doc-base <arg>       Application doc path
+ -h,--help                 Print help
+ -p,--tomcat-port <arg>    Tomcat port number
+ -s,--solr-home <arg>      Solr home path
+ -t,--tomcat-base <arg>    Tomcat base path
+
+$ bin/solrserver --solr-home=classes/META-INF/solr-server/solr
+...
+INFO: Starting ProtocolHandler ["http-nio-8080"]
+2018-10-21 23:17:42,778 [main] INFO  com.github.woonsan.solr.server.tomcat.launch.Main - Tomcat has started and is waiting for requests.
+```
 
 ## How to test
 
     $ curl -v http://localhost:8080/hello/
-
--- src/main/java/com/github/woonsan/solr/server/tomcat/launch/Main.java
+++ src/main/java/com/github/woonsan/solr/server/tomcat/launch/Main.java
@@ -24,6 +24,14 @@ import org.apache.catalina.LifecycleEvent;
 import org.apache.catalina.LifecycleListener;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.solr.servlet.SolrDispatchFilter;
+import org.apache.tomcat.util.descriptor.web.FilterDef;
+import org.apache.tomcat.util.descriptor.web.FilterMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,20 +41,49 @@ public class Main {
 
     private static Logger log = LoggerFactory.getLogger(Main.class);
 
+    private static Options cliOptions = createCLIOptions();
+
     private static final LifecycleListener noopLifecycleListener = new LifecycleListener() {
         public void lifecycleEvent(LifecycleEvent event) {
         }
     };
 
     public static void main(String[] args) throws Exception {
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine cmd = parser.parse(cliOptions, args);
+
+        if (cmd.hasOption("help")) {
+            new HelpFormatter().printHelp("solrserver", cliOptions);
+            System.exit(1);
+        }
+
+        final String solrHome = cmd.getOptionValue("solr-home", null);
+        final String tomcatBase = cmd.getOptionValue("tomcat-base", "tomcat-base");
+        final String tomcatPort = cmd.getOptionValue("tomcat-port", "8080");
+        final String contextPath = cmd.getOptionValue("context-path", "");
+        final String docBase = cmd.getOptionValue("doc-base", tomcatBase + "/webapp");
+
+        if (solrHome == null || solrHome.isEmpty()) {
+            new HelpFormatter().printHelp("solrserver", cliOptions);
+            System.exit(1);
+        }
+
+        final File solrHomeDir = new File(solrHome);
+        if (!solrHomeDir.isDirectory()) {
+            solrHomeDir.mkdirs();
+        }
+        System.setProperty("solr.solr.home", solrHomeDir.getAbsolutePath());
+
+        final File tomcatBaseDir = new File(tomcatBase);
+        if (!tomcatBaseDir.isDirectory()) {
+            tomcatBaseDir.mkdirs();
+        }
+        final File appDocBaseDir = new File(docBase);
+        if (!appDocBaseDir.isDirectory()) {
+            appDocBaseDir.mkdirs();
+        }
 
         System.setProperty("org.apache.catalina.startup.EXIT_ON_INIT_FAILURE", "true");
-
-        final File tomcatBase = new File("tomcat");
-        final File docBaseDir = new File(tomcatBase, "webapp");
-        if (!docBaseDir.isDirectory()) {
-            docBaseDir.mkdirs();
-        }
 
         final Tomcat tomcat = new Tomcat() {
             @Override
@@ -55,21 +92,36 @@ public class Main {
             }
         };
 
-        tomcat.setPort(8080);
-        tomcat.setBaseDir(tomcatBase.getAbsolutePath());
+        tomcat.setPort(Integer.parseInt(tomcatPort));
+        tomcat.setBaseDir(tomcatBaseDir.getAbsolutePath());
 
         // Note: make sure that tomcat creates the default connector...
         tomcat.getConnector();
 
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("", docBaseDir.getAbsolutePath());
+        StandardContext ctx = (StandardContext) tomcat.addWebapp(contextPath, appDocBaseDir.getAbsolutePath());
         ctx.setParentClassLoader(Main.class.getClassLoader());
 
+        addSolrRequestFilter(ctx);
         addServletAndMappings(ctx, "HelloServlet", new HelloServlet(), "/hello/*");
 
         tomcat.start();
         log.info("Tomcat has started and is waiting for requests.");
 
         tomcat.getServer().await();
+    }
+
+    private static void addSolrRequestFilter(final StandardContext ctx) {
+        final FilterDef filterDef = new FilterDef();
+        filterDef.setFilterName("SolrRequestFilter");
+        filterDef.setFilter(new SolrDispatchFilter());
+        filterDef.addInitParameter("excludePatterns", "/partials/.+,/libs/.+,/css/.+,/js/.+,/img/.+,/tpl/.+");
+
+        final FilterMap filterMap = new FilterMap();
+        filterMap.setFilterName("SolrRequestFilter");
+        filterMap.addURLPatternDecoded("/*");
+
+        ctx.addFilterDef(filterDef);
+        ctx.addFilterMap(filterMap);
     }
 
     private static void addServletAndMappings(final StandardContext ctx, final String servletName,
@@ -83,5 +135,16 @@ public class Main {
         }
 
         log.info("{} servlet added: {}, with mappings: {}", servletName, servlet, decodedMappings);
+    }
+
+    private static Options createCLIOptions() {
+        Options options = new Options();
+        options.addOption("h", "help", false, "Print help");
+        options.addOption("s", "solr-home", true, "Solr home path");
+        options.addOption("t", "tomcat-base", true, "Tomcat base path");
+        options.addOption("p", "tomcat-port", true, "Tomcat port number");
+        options.addOption("c", "context-path", true, "Application context path");
+        options.addOption("d", "doc-base", true, "Application doc path");
+        return options;
     }
 }
