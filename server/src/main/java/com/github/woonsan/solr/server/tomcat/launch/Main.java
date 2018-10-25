c07dadeaffdd0ab2bf0c5377b43c5ae468bba3d7
Woonsan Ko <woonsan@apache.org>
2018-10-25 16:22:07 -0400

    Setting default port to 8983 and contextPath to /solr

--- server/src/main/java/com/github/woonsan/solr/server/tomcat/launch/Main.java
+++ server/src/main/java/com/github/woonsan/solr/server/tomcat/launch/Main.java
@@ -60,8 +60,8 @@ public class Main {
 
         final String solrHome = cmd.getOptionValue("solr-home", null);
         final String tomcatBase = cmd.getOptionValue("tomcat-base", "target/temp-tomcat-base");
-        final String tomcatPort = cmd.getOptionValue("tomcat-port", "8080");
-        final String contextPath = cmd.getOptionValue("context-path", "");
+        final String tomcatPort = cmd.getOptionValue("tomcat-port", "8983");
+        final String contextPath = cmd.getOptionValue("context-path", "/solr");
         final String docBase = cmd.getOptionValue("doc-base", tomcatBase + File.separator + "webapp");
         final String logDir = cmd.getOptionValue("log-dir", tomcatBase + File.separator + "logs");
 
